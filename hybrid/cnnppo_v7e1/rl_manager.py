2dd6abd068a76b87ec97adcfb7861876829a8d3e
jxinnan <jiangxinnan416@gmail.com>
2025-05-26 16:55:28 +0800

    Use CPU torch when necessary

--- hybrid/cnnppo_v7e1/rl_manager.py
+++ hybrid/cnnppo_v7e1/rl_manager.py
@@ -1,8 +1,11 @@
+import torch
+
 from .CNNPPOPlayer import CNNPPOPlayer
 
 class RLManager:
     def __init__(self):
-        self.model = CNNPPOPlayer("hybrid/cnnppo_v7e1/v7e1_ep31600.pth")
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model = CNNPPOPlayer("hybrid/cnnppo_v7e1/v7e1_ep31600.pth", device)
 
     def rl(self, observation: dict[str, int | list[int]]) -> int:
         return self.model(observation)
--- hybrid/cnnppo_v7e7/rl_manager.py
+++ hybrid/cnnppo_v7e7/rl_manager.py
@@ -1,8 +1,11 @@
+import torch
+
 from .CNNPPOPlayer import CNNPPOPlayer
 
 class RLManager:
     def __init__(self):
-        self.model = CNNPPOPlayer("hybrid/cnnppo_v7e7/v7e7_ep1200.pth")
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model = CNNPPOPlayer("hybrid/cnnppo_v7e7/v7e7_ep1200.pth", device)
 
     def rl(self, observation: dict[str, int | list[int]]) -> int:
         return self.model(observation)
