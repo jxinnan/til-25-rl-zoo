a3776e84bd4b8a12f5744abc0889daee5951b7e1
jxinnan <jiangxinnan416@gmail.com>
2025-05-27 16:07:56 +0800

    Fixed autorun script

--- test_solo_scout_all.sh
+++ test_solo_scout_all.sh
@@ -1,7 +1,15 @@
-for d in scouts; do
-    uv run test_solo_scout.py -g $d
+#!/bin/bash
+
+dir=$(pwd)
+scoutpath=$dir/scouts
+hybridpath=$dir/hybrid
+
+for d in "$scoutpath"/*; do
+    echo ${d##*/}
+    uv run test_solo_scout.py -g ${d##*/}
 done
 
-for d in hybrid; do
-    uv run test_solo_scout.py -g --hybrid $d
+for d in "$hybridpath"/*; do
+    echo ${d##*/}
+    uv run test_solo_scout.py -g --hybrid ${d##*/}
 done
